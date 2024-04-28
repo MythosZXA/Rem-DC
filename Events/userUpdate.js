6c90163a637ed666620a8f73d045eeff1b01040d
MythosZXA <ubw.zeroxa@gmail.com>
2024-04-27 19:49:21 -0500

    * removed logic for user update event

--- Events/userUpdate.js
+++ Events/userUpdate.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: 'userUpdate',
 	many: true,
 	execute(oldUser, newUser, rem) {
-		const user = rem.remDB.get('users').get(newUser.id);
-		user.username = newUser.username;
+		
 	}
 };

d098cac25899a1c205e7ab55915a458056b8a563
MythosZXA <ubw.zeroxa@gmail.com>
2024-08-05 17:41:39 -0500

    * updated couple endpoints to use new sesion cookie * swapped those endpoints to have guard clauses

--- Endpoints/serverMembers.js
+++ Endpoints/serverMembers.js
@@ -1,14 +1,15 @@
 module.exports = {
-	name: '/serverMembers',
-	type: 'get',
-	async execute(req, res, rem, expressGlobal) {
-		if (expressGlobal.admins.has(req.cookies.sessionID)) {
-			const server = await rem.guilds.fetch(process.env.guildId);
-			const members = server.members.cache;
-			const realMembers = members.filter(member => !member.user.bot);
-			res.send({ remAvatarURL: rem.user.displayAvatarURL(), members: realMembers });
-		} else {
-			res.sendStatus(401);
-		}
-	}
+  name: '/serverMembers',
+  type: 'get',
+  async execute(req, res, rem, expressGlobal) {
+    if (!expressGlobal.admins.has(req.cookies.rdcSID)) {
+      res.sendStatus(401);
+      return;
+    }
+    
+    const server = await rem.guilds.fetch(process.env.guildId);
+    const members = server.members.cache;
+    const realMembers = members.filter(member => !member.user.bot);
+    res.send({ remAvatarURL: rem.user.displayAvatarURL(), members: realMembers });
+  }
 };
--- Endpoints/textChannels.js
+++ Endpoints/textChannels.js
@@ -1,14 +1,17 @@
+const { ChannelType } = require("discord.js");
+
 module.exports = {
-	name: '/textChannels',
-	type: 'get',
-	async execute(req, res, rem, expressGlobal) {
-		if (expressGlobal.admins.has(req.cookies.sessionID)) {
-			const server = await rem.guilds.fetch(process.env.guildId);
-			const channels = await server.channels.fetch();
-			const textChannels = channels.filter(channel => channel.type === 'GUILD_TEXT');
-			res.send( textChannels );
-		} else {
-			res.sendStatus(401);
-		}
-	}
+  name: '/textChannels',
+  type: 'get',
+  async execute(req, res, rem, expressGlobal) {
+    if (!expressGlobal.admins.has(req.cookies.rdcSID)) {
+      res.sendStatus(401);
+      return;
+    }
+    
+    const server = await rem.guilds.fetch(process.env.guildId);
+    const channels = await server.channels.fetch();
+    const textChannels = channels.filter(channel => channel.type === ChannelType.GuildText);
+    res.send(textChannels);
+  }
 };

e50eff264a8d3d5cda433bbc6ecacab6e880d6d3
MythosZXA <ubw.zeroxa@gmail.com>
2024-08-10 21:42:19 -0500

    * updated/refactored card lobby

--- Endpoints/cards/lobby.js
+++ Endpoints/cards/lobby.js
@@ -1,3 +1,5 @@
+const epFuncs = require ('../common');
+
 const lobby = new Map(); // holds objects representing guests in the cards lobby
 
 /**
@@ -5,11 +7,10 @@ const lobby = new Map(); // holds objects representing guests in the cards lobby
  */
 function sendLobby() {
   lobby.forEach(curGuest => {
-    const filteredLobby = Array.from(lobby.values())
+    const arrParticipants = Array.from(lobby.values())
       .map(guest => guest.participant) // exclude res
-      .filter(otherGuest => otherGuest.username !== curGuest.participant.username); // exclude "self"
 
-    curGuest.res.write(`data: ${JSON.stringify(filteredLobby)}\n\n`);
+    curGuest.res.write(`data: ${JSON.stringify(arrParticipants)}\n\n`);
   });
 }
 
@@ -17,42 +18,40 @@ module.exports = {
   name: '/cards/lobby',
   type: 'get',
   async execute(req, res, rem) {
-    if (!req.cookies.sessionID || !req.cookies.dcUsername) {
-      res.sendStatus(403);
-      return;
-    }
+    const SID = epFuncs.extractSID(req, res);
+    if (!SID) return;
 
-    const sessID = req.cookies.sessionID;
-    const username = req.cookies.dcUsername;
+    const sessUser = epFuncs.getSessUser(SID, res, rem.express);
+    if (!sessUser) return;
 
-    // send current lobby to guest who just joined the lobby
+    // join lobby
+    const server = await rem.guilds.fetch(process.env.guildId);
+    const member = server.members.cache.find(member => member.user.username === sessUser.username);
+    const guestObj = {
+      participant: {
+        avatarURL: member.displayAvatarURL(),
+        username: sessUser.username
+      },
+      res
+    };
+    lobby.set(SID, guestObj);
+    console.log(`${sessUser.username} has joined the cards lobby`);
+
+    // set response
     res.writeHead(200, {
       'Content-Type': 'text/event-stream',
       'Connection': 'keep-alive',
       'Cache-Control': 'no-cache'
     });
 
-    // join lobby
-    const server = await rem.guilds.fetch(process.env.guildId);
-    const member = server.members.cache.find(member => member.user.username === username);
-    const guestObj = {
-      participant: {
-        avatarURL: member.displayAvatarURL(),
-        username
-      },
-      res
-    };
-    lobby.set(sessID, guestObj);
-    console.log(`${username} has joined the cards lobby`);
-
     // send updated lobby to guest in lobby
     sendLobby();
 
     // leave lobby
     req.on('close', () => {
-      lobby.delete(sessID);
+      lobby.delete(SID);
       sendLobby();
-      console.log(`${username} has left the cards lobby`);
+      console.log(`${sessUser.username} has left the cards lobby`);
     });
   }
 };

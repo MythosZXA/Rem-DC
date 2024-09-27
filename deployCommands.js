968804b556089226c40bbc9bd797209e6ef5e26e
MythosZXA <ubw.zeroxa@gmail.com>
2024-09-27 00:33:42 -0500

    * updated command deploy file to function with discord.js v14

--- deployCommands.js
+++ deployCommands.js
@@ -1,25 +1,24 @@
 import 'dotenv/config';
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v9';
+import { REST, Routes } from 'discord.js';
 import fs from 'fs';
 
 const commands = [];
 const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
+
 (async () => {
   for (const file of commandFiles) {
     const command = (await import(`./Commands/${file}`)).default;
     commands.push(command.data.toJSON());
   }
-})();
 
-const rest = new REST({ version: '9' }).setToken(process.env.token);
-(async () => {
+  const rest = new REST({ version: '10' }).setToken(process.env.token);
+
   try {
     console.log('Started refreshing application (slash) commands.');
 
     await rest.put(
       Routes.applicationGuildCommands(process.env.clientId, process.env.guildId),
-      { body: commands },
+      { body: commands }
     );
 
     console.log('Successfully reloaded application (slash) commands.');
